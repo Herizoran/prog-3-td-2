af925a48893a82da4ff37c3cb1bea67b949b6009
herizoran <herizo.ran@gmail.com>
2022-12-15 17:24:33 +0300

    Empty commit

---
+++ src/main/java/app/prog3td2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package app.prog3td2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Prog3Td2Application.class);
+    }
+
+}
