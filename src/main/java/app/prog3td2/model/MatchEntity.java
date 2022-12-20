85000e7c379c64697b51dde7eb0aac957ee31ca2
herizoran <herizo.ran@gmail.com>
2022-12-20 08:19:01 +0300

    feat: add match entity

---
+++ src/main/java/app/prog3td2/model/MatchEntity.java
@@ -0,0 +1,27 @@
+package app.prog3td2.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "team")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    /*
+    @ManyToMany(mappedBy = "team")
+    private List<SponsorEntity> sponsor;
+     */
+}
